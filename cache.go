aa651164017513efb3c033fde899aaba4054754e
troxanna <anevodchikova@Noutbuk-Anastasia.local>
2025-01-04 13:36:43 +0300

    add ttl and mutex

--- cache.go
+++ cache.go
@@ -2,29 +2,43 @@ package cache
 
 import (
 	"errors"
+	"sync"
+	"time"
 )
 
+type ItemCache struct {
+	value interface{}
+	ttl time.Duration
+	createdAt time.Time
+}
+
 type Cache struct {
-	storage map[string]interface{}
+	storage map[string]ItemCache
+	mu *sync.RWMutex
 }
 
 func New() *Cache {
 	return &Cache{
-		storage: make(map[string]interface{}),
+		storage: make(map[string]ItemCache),
 	}
 }
 
-func (c *Cache) Get(key string) interface{} {
+func (c *Cache) Get(key string) (interface{}, error) {
 	value := c.storage[key]
-	return value
+	if value.createdAt.UnixMicro() < time.Now().UnixMicro() - value.ttl.Microseconds() {
+		return ItemCache{}, errors.New("value for this key already exists")
+	}
+	return value.value, nil
 }
 
-func (c *Cache) Set(key string, value interface{}) (bool, error) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) (bool, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	existsValue := c.storage[key]
-	if existsValue != nil {
+	if existsValue.value != nil {
 		return false, errors.New("value for this key already exists")
 	}
-	c.storage[key] = value
+	c.storage[key] = ItemCache{value: value, ttl: ttl, createdAt: time.Now()}
 	return true, nil
 }
 
