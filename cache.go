3ca13f06d5da5f17856d9bde9d55864fb777decd
troxanna <anevodchikova@Noutbuk-Anastasia.local>
2025-01-04 14:53:08 +0300

    implement backgroundCacheCleaner

--- cache.go
+++ cache.go
@@ -8,43 +8,65 @@ import (
 
 type ItemCache struct {
 	value interface{}
-	ttl time.Duration
-	createdAt time.Time
+	expiredAt time.Time
 }
 
 type Cache struct {
-	storage map[string]ItemCache
-	mu *sync.RWMutex
+	storage sync.Map
 }
 
 func New() *Cache {
-	return &Cache{
-		storage: make(map[string]ItemCache),
-		mu: new(sync.RWMutex),
+	storage := &Cache{
+		storage: sync.Map{},
 	}
+	go storage.backgroundCacheCleaner()
+
+	return storage
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
-	value := c.storage[key]
-	if value.createdAt.UnixMicro() < time.Now().UnixMicro() - value.ttl.Microseconds() {
-		return ItemCache{}, errors.New("value for this key already exists")
+	load, ok := c.storage.Load(key)
+	if !ok {
+		return false, errors.New("value for this key not exists")
+	}
+	value, ok := load.(ItemCache)
+	if !ok {
+		return false, errors.New("type assertion error")
 	}
 	return value.value, nil
 }
 
 func (c *Cache) Set(key string, value interface{}, ttl time.Duration) (bool, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	existsValue := c.storage[key]
-	if existsValue.value != nil {
+	_, ok := c.storage.Load(key)
+	if ok {
 		return false, errors.New("value for this key already exists")
 	}
-	c.storage[key] = ItemCache{value: value, ttl: ttl, createdAt: time.Now()}
+	c.storage.Store(key, ItemCache{value: value, expiredAt: time.Now().Add(ttl)})
 	return true, nil
 }
 
 func (c *Cache) Delete(key string) {
-	delete(c.storage, key)
+	c.storage.Delete(key)
 }
 
+func (c *Cache) backgroundCacheCleaner() {
+	timer := time.NewTicker(time.Second * 1)
+	defer timer.Stop()
+
+	for {
+		<- timer.C
+		c.storage.Range(func(key, v interface{}) bool {
+			value, ok := v.(ItemCache)
+			if !ok {
+				return false
+			}
+			if time.Now().After(value.expiredAt) {
+				c.storage.Delete(key)
+				return true
+			}
+			return true
+		})
+	}
+
+}
 
